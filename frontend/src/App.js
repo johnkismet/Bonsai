56a1717fa2b8e77ede528a72e1bca160c582be7f
johnkismet <mojomanftw@gmail.com>
2020-12-23 00:08:48 -0500

    Very basic skeleton of main page made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+// import Button from "@material-ui/core/Button";
+import { Grid } from "@material-ui/core";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<nav>
+				<div id="burgerMenu">
+					<span className="burgerLine"></span>
+					<span className="burgerLine"></span>
+					<span className="burgerLine"></span>
+				</div>
+
+				<h1 id="navHeader">BONSAI</h1>
+			</nav>
+
+			<div id="treeFarm">
+				<Grid container direction="row" justify="center" alignItems="center">
+					<div className="tree">Tree 1</div>
+					<div className="tree">Tree 2</div>
+					<div className="tree">Tree 3</div>
+					<div className="tree">Tree 4</div>
+					<div className="tree">Tree 5</div>
+					<div className="tree">Tree 6</div>
+					<div className="tree">Tree 7</div>
+					<div className="tree">Tree 8</div>
+				</Grid>
+			</div>
 		</div>
 	);
 }
