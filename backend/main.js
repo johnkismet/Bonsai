934ac048fd10744a48461b21d3dc6578fb96ba54
johnkismet <mojomanftw@gmail.com>
2020-12-31 17:02:26 -0500

    Successfully connecting to MongoDB and able to send tree schemas to db

--- backend/main.js
+++ backend/main.js
@@ -1,1 +1,46 @@
-// todo: make app
+// import cors from "cors";
+// import path from "path";
+import express from "express";
+const mongoose = require("mongoose");
+const app = express();
+const uri =
+	"mongodb+srv://Kismet:wZ0vNyvkUENVhg2o@cluster0.l8p7d.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+
+// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(
+	uri,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => {
+		console.log("Connected with Mongo");
+	}
+);
+
+const treeSchema = new mongoose.Schema({
+	name: String,
+});
+const Tree = mongoose.model("Tree", treeSchema);
+
+app.get("/trees", (req, res) => {
+	Tree.find((err, trees) => {
+		if (err) return console.error(err);
+		res.send(trees);
+	});
+});
+
+app.post("/trees", (req, res) => {
+	let treeName = req.body.name;
+	const bonsai = new Tree({
+		name: treeName,
+	});
+	bonsai.save((err) => {
+		if (err) return console.error(err);
+	});
+	res.send(`${treeName} was posted!`);
+});
+
+app.listen(4000, () => {
+	console.log("express server is running on port 4000");
+});
