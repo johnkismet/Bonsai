90823b1dfb2344dab20711e9e18db6461f2b89e0
NicholasSmock <cheesbat@aol.com>
2021-01-05 14:06:53 -0500

    added non functional login component, moved old home page to /treefarm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,38 +10,42 @@ import Store from "./components/Store/Store";
 import Statistics from "./components/Statistics/Statistics";
 import Archive from "./components/Archive/Archive";
 import Sidebar from "./components/sidebar/Sidebar";
+import Login from "./components/Login/Login";
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Sidebar pageWrapId={"page-wrap"} outerContainerId={"App"} />
-				<Switch>
-					<Route exact path="/">
-						<TreeFarm />
-					</Route>
-					<Route exact path="/store">
-						<Store />
-					</Route>
-					<Route exact path="/archive">
-						<Archive />
-					</Route>
-					<Route exact path="/statistics">
-						<Statistics />
-					</Route>
-					<Route exact path="/newTree">
-						<NewTree />
-					</Route>
-					<Route exact path="/trees/:id">
-						<Tree />
-					</Route>
-					<Route path="*">
-						<MissingPage />
-					</Route>
-				</Switch>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        <Sidebar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/treefarm">
+            <TreeFarm />
+          </Route>
+          <Route exact path="/store">
+            <Store />
+          </Route>
+          <Route exact path="/archive">
+            <Archive />
+          </Route>
+          <Route exact path="/statistics">
+            <Statistics />
+          </Route>
+          <Route exact path="/newTree">
+            <NewTree />
+          </Route>
+          <Route exact path="/trees/:id">
+            <Tree />
+          </Route>
+          <Route path="*">
+            <MissingPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login(props) {
+  return (
+    <>
+      <button>Login</button>
+      <button>Register</button>
+    </>
+  );
+}
+export default Login;
