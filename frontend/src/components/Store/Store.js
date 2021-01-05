cea00e84792713d267cf02135050073b45522643
NicholasSmock <cheesbat@aol.com>
2021-01-05 09:24:05 -0500

    resolving merge conflict

--- frontend/src/components/Store/Store.js
+++ frontend/src/components/Store/Store.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Store.css";
 
 function Store(props) {
-	return (
-		<div className="Store">
-			<h1>Store</h1>
-			<h2>(Under Construction)</h2>
-		</div>
-	);
+  return (
+    <div className="Store">
+      <h1>Store</h1>
+      <h2>(Under Construction)</h2>
+    </div>
+  );
 }
 export default Store;
