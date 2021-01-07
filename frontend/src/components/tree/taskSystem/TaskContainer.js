a587b9e5fa494ba8f980e2891bbc2d029dc1090e
NicholasSmock <cheesbat@aol.com>
2021-01-07 09:51:27 -0500

    started work on addTask function

--- frontend/src/components/tree/taskSystem/TaskContainer.js
+++ frontend/src/components/tree/taskSystem/TaskContainer.js
@@ -5,63 +5,71 @@ import Task from "./Task";
 const axios = require("axios").default;
 
 let tasksArray = [
-	{
-		name: "task one",
-		parentId: "12312",
-		completed: false,
-	},
-	{
-		name: "task two",
-		parentId: "123311",
-		completed: false,
-	},
-	{
-		name: "task three",
-		parentId: "234342",
-		completed: false,
-	},
+  {
+    name: "task one",
+    parentId: "12312",
+    completed: false,
+  },
+  {
+    name: "task two",
+    parentId: "123311",
+    completed: false,
+  },
+  {
+    name: "task three",
+    parentId: "234342",
+    completed: false,
+  },
 ];
 
 function TaskContainer(props) {
-	let id = props.id;
-	const [tasks, setTasks] = useState([]);
+  let id = props.id;
+  const [tasks, setTasks] = useState([]);
+  const addTask = () => {
+    let newTask = {
+      name: "if you see me, scream in excitement",
+      parentId: id,
+      completed: false,
+    };
+    tasksArray.push(newTask);
+  };
 
-	const showTasks = tasksArray.map((task, index) => (
-		<Task className="task" name={task.name} completed={task.completed} />
-	));
+  const showTasks = tasksArray.map((task, index) => (
+    <Task className="task" name={task.name} completed={task.completed} />
+  ));
 
-	useEffect(() => {
-		axios
-			.get(`http://localhost:4000/getTasks/${id}`)
-			.then(function (res) {
-				// handle success
-				let data = res.data;
-				setTasks(data);
-			})
-			.catch(function (error) {
-				// handle error
-				console.error(error);
-			})
-			.then(function () {
-				// always executed
-			});
-	}, []);
-	return (
-		<div className="Tasks">
-			<div className="inputContainer">
-				<input
-					placeholder="What're you working on?"
-					className="taskInput"
-				></input>
-				<input className="submitTask" type="submit" />
-			</div>
-			<div className="taskContainer">
-				<Grid container direction="column" justify="center" alignItems="center">
-					{showTasks}
-				</Grid>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/getTasks/${id}`)
+      .then(function (res) {
+        // handle success
+        let data = res.data;
+        setTasks(data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.error(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }, []);
+  return (
+    <div className="Tasks">
+      <div className="inputContainer">
+        <input
+          placeholder="What're you working on?"
+          className="taskInput"
+        ></input>
+        <input className="submitTask" onClick={() => addTask()} type="button" />
+      </div>
+      <div className="taskContainer">
+        <Grid container direction="column" justify="center" alignItems="center">
+          {showTasks}
+        </Grid>
+      </div>
+    </div>
+  );
 }
 
 export default TaskContainer;
