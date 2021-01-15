0c4ebf5d362c23c630cb14709e9030bfb9fa50eb
Timothy Charboneau <charboneau@protonmail.ch>
2021-01-15 13:48:40 -0500

    tuned up the styling a bunch

--- src/components/tree/progressBar/ProgressBar.js
+++ src/components/tree/progressBar/ProgressBar.js
@@ -1,16 +1,22 @@
 import "./ProgressBar.css";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default function ProgressBar(props) {
-    console.log(props.points)
+  console.log(props.points);
   return (
-      
     <>
       <h2>Progress</h2>
-      <div className="progressBar">
-      
-        <div className="progress" style={{"width": props.points + "%"}}></div>
-        {`${props.points}%`}
-      </div>
+      {props.points ? (
+        <div className="progressBar">
+          <div className="progress" style={{ width: props.points + "%" }}></div>
+          {`${props.points}%`}
+        </div>
+      ) : (
+        <div className="progressBar">
+          <CircularProgress className="spinner" size="3vh"/>
+          Loading...
+        </div>
+      )}
     </>
   );
 }
