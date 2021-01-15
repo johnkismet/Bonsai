76d6503935c375ee04ef17a89f1339678f47aaef
Timothy Charboneau <charboneau@protonmail.ch>
2021-01-15 14:21:28 -0500

    tweaked percentage to pop out of progress when high

--- src/components/tree/progressBar/ProgressBar.js
+++ src/components/tree/progressBar/ProgressBar.js
@@ -3,20 +3,49 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default function ProgressBar(props) {
   console.log(props.points);
-  return (
-    <>
-      <h2>Progress</h2>
-      {props.points ? (
-        <div className="progressBar">
-          <div className="progress" style={{ width: props.points + "%" }}></div>
-          {`${props.points}%`}
-        </div>
-      ) : (
-        <div className="progressBar">
-          <CircularProgress className="spinner" size="3vh"/>
-          Loading...
-        </div>
-      )}
-    </>
-  );
+  if (props.points < 60) {
+    return (
+        <>
+          <h2>Progress</h2>
+          {props.points ? (
+            <>
+              <div className="progressBar">
+                <div className="progress" style={{ width: props.points + "%" }}>
+                  
+                </div>
+                <span className="points">{`${props.points}%`}</span>
+              </div>
+            </>
+          ) : (
+            <div className="progressBar">
+              <CircularProgress className="spinner" size="3vh" />
+              Loading...
+            </div>
+          )}
+        </>
+      );
+  } else {
+      console.log("smol")
+      return (
+        <>
+        <h2>Progress</h2>
+        {props.points ? (
+          <>
+            <div className="progressBar">
+              <div className="progress" style={{ width: props.points + "%" }}>
+              <span className="points">{`${props.points}%`}</span>
+              </div>
+              
+            </div>
+          </>
+        ) : (
+          <div className="progressBar">
+            <CircularProgress className="spinner" size="3vh" />
+            Loading...
+          </div>
+        )}
+      </>
+      )
+  }
+  
 }
