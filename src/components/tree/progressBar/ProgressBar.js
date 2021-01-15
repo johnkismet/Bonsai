b5959cdc6dfb7ba20869984af749e3131087f405
Timothy Charboneau <charboneau@protonmail.ch>
2021-01-15 14:22:21 -0500

    recommit due to bad save

--- src/components/tree/progressBar/ProgressBar.js
+++ src/components/tree/progressBar/ProgressBar.js
@@ -5,37 +5,16 @@ export default function ProgressBar(props) {
   console.log(props.points);
   if (props.points < 60) {
     return (
-        <>
-          <h2>Progress</h2>
-          {props.points ? (
-            <>
-              <div className="progressBar">
-                <div className="progress" style={{ width: props.points + "%" }}>
-                  
-                </div>
-                <span className="points">{`${props.points}%`}</span>
-              </div>
-            </>
-          ) : (
-            <div className="progressBar">
-              <CircularProgress className="spinner" size="3vh" />
-              Loading...
-            </div>
-          )}
-        </>
-      );
-  } else {
-      console.log("smol")
-      return (
-        <>
+      <>
         <h2>Progress</h2>
         {props.points ? (
           <>
             <div className="progressBar">
-              <div className="progress" style={{ width: props.points + "%" }}>
+              <div
+                className="progress"
+                style={{ width: props.points + "%" }}
+              ></div>
               <span className="points">{`${props.points}%`}</span>
-              </div>
-              
             </div>
           </>
         ) : (
@@ -45,7 +24,27 @@ export default function ProgressBar(props) {
           </div>
         )}
       </>
-      )
+    );
+  } else {
+    console.log("smol");
+    return (
+      <>
+        <h2>Progress</h2>
+        {props.points ? (
+          <>
+            <div className="progressBar">
+              <div className="progress" style={{ width: props.points + "%" }}>
+                <span className="points">{`${props.points}%`}</span>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="progressBar">
+            <CircularProgress className="spinner" size="3vh" />
+            Loading...
+          </div>
+        )}
+      </>
+    );
   }
-  
 }
