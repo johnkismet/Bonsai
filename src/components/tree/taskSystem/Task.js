3dac89100941d9761f6b1e1665f9281df3c0ab1c
johnkismet <mojomanftw@gmail.com>
2021-01-18 16:43:26 -0500

    Weird merging issue, app is now completely functional and ready for demo

--- src/components/tree/taskSystem/Task.js
+++ src/components/tree/taskSystem/Task.js
@@ -1,22 +1,32 @@
 import "./Tasks.css";
 import React from "react";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function Task(props) {
-  return (
-    <div className="task">
-      {/* <div className="textSpacer"></div> */}
-      <p>{props.name}</p>
-      <button onClick={() => props.Delete(props.taskId)}>Delete</button>
-      <input
-        className="completedBtn"
-        onChange={() => props.onChange(props.taskId)}
-        type="checkbox"
-        checked={props.completed}
-      />
-      {/* <div className="textSpacer"></div> */}
-    </div>
-  );
+	return (
+		<div className="task">
+			{/* <div className="textSpacer"></div> */}
+			<p>{props.name}</p>
+			<div className="actions">
+				<FontAwesomeIcon
+					icon={faTrash}
+					onClick={() => props.Delete(props.taskId)}
+				>
+					Delete
+				</FontAwesomeIcon>
+				<input
+					className="completedBtn"
+					onChange={() => props.onChange(props.taskId)}
+					type="checkbox"
+					checked={props.completed}
+				/>
+			</div>
+
+			{/* <div className="textSpacer"></div> */}
+		</div>
+	);
 }
 
 export default Task;
